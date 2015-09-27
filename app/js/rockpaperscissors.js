9495203c42efcab72549afc68ea14d45f7ba648e
Andrew Anissi <andrew@wingedearth.com>
2015-09-27 11:22:37 -0700

    cleaned up rockpaperscissors.js

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,69 +22,57 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    return playerMove || getInput();
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
+    return move || getInput();
 }
 
 function getComputerMove(move) {  
-    return computerMove || randomPlay();
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove)
 {
     var winner;
-    console.log("Player chooses", playerMove);
-    console.log("Computer chooses", computerMove);
+    console.log("The human player chooses", playerMove);
+    console.log("The computer chooses", computerMove);
     if (playerMove==="rock")
-        {
-        if (computerMove=="rock") {
-            winner = "tie"; }
-        else if (computerMove=="scissors") {
-            winner = "computer"; }
-        else {
-            winner = "player"; }
-        }
+    {
+        if (computerMove=="rock")
+            winner = "tie";
+        else if (computerMove=="scissors")
+            winner = "computer";
+        else
+            winner = "player";
+    }
 
     if (playerMove==="scissors")
-        {
-        if (computerMove=="scissors") {
-            winner = "tie"; }
-        else if (computerMove=="rock") {
-            winner = "computer"; }
-        else {
-            winner = "player"; }
-        }
+    {
+        if (computerMove=="scissors")
+            winner = "tie";
+        else if (computerMove=="rock")
+            winner = "computer";
+        else
+            winner = "player";
+    }
     
-        if (playerMove==="paper")
-        {
-        if (computerMove=="paper") {
-            winner = "tie"; }
-        else if (computerMove=="scissors") {
-            winner = "computer"; }
-        else {
-            winner = "player"; }
-        }
-
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove==="paper")
+    {
+        if (computerMove=="paper")
+            winner = "tie";
+        else if (computerMove=="scissors")
+            winner = "computer";
+        else
+            winner = "player";
+    }
     return winner;
 }
 
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
+    console.log("Let's play Rock, Paper, Scissors, if you dare.");
     var playerWins = 0;
     var computerWins = 0;
     var win;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    while (1>0) {
+
+    while (1>0) {  // infinite loop with an exit condition.
         win = getWinner(getPlayerMove(), getComputerMove() );
             if (win == "player")
                 playerWins = playerWins + 1;
@@ -93,8 +81,8 @@ function playToFive() {
             if (win == "tie")
                 console.log("It's a tie!!");
         console.log("Winner this round:",win);
-        console.log("player:", playerWins);
-        console.log("computer:", computerWins);
+        console.log("Human player:", playerWins);
+        console.log("Computer:", computerWins);
         if ((playerWins>=5) || (computerWins>=5))
         {
             console.log("Overall Winner:", win);
